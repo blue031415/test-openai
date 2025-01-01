58f8e24f1d1157fffb18e089d05e5d7206121783
blue031415 <blue031415@gmail.com>
2025-01-01 22:06:54 +0900

    feat : api叩けた

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=API_KEY)
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini", messages=[{"role": "user", "content": "俳句を一つ詠んで"}]
+)
+
+print(completion.choices[0].message.content)
